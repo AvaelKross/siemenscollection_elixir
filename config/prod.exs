712b8cfc41cfd82638cd33468e01794c507de939
Avael <avaelkross@gmail.com>
2016-05-03 14:00:17 +0500

    server:true

--- config/prod.exs
+++ config/prod.exs
@@ -14,7 +14,8 @@ use Mix.Config
 config :siemens_collection, SiemensCollection.Endpoint,
   http: [port: 8888],
   url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/manifest.json"
+  cache_static_manifest: "priv/static/manifest.json",
+  server: true
 
 # Do not print debug messages in production
 config :logger, level: :info
