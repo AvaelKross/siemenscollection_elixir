61fd329ae305001b31c0f5c0865fe74b0ee7b098
Avael <avaelkross@gmail.com>
2016-07-15 10:48:16 +0200

    limits for IM

--- web/uploaders/image.ex
+++ web/uploaders/image.ex
@@ -12,15 +12,15 @@ defmodule SiemensCollection.Image do
   # Define a thumbnail transformation:
 
   def transform(:original, _) do
-    {:convert, "-auto-orient"}
+    {:convert, "-limit memory 64 -limit map 128 -auto-orient"}
   end
 
   def transform(:thumb, _) do
-    {:convert, "-auto-orient -thumbnail 250x250> -gravity center -format jpg", :jpg}
+    {:convert, "-limit memory 64 -limit map 128 -auto-orient -thumbnail 250x250> -gravity center -format jpg", :jpg}
   end
 
   def transform(:popup, _) do
-    {:convert, "-auto-orient -thumbnail 2000x1200> -gravity center -format jpg", :jpg}
+    {:convert, "-limit memory 64 -limit map 128 -auto-orient -thumbnail 2000x1200> -gravity center -format jpg", :jpg}
   end
 
   # Override the persisted filenames:
