d838a5415dc612349aec292cfce13a8b7c191e80
Avael <avaelkross@gmail.com>
2016-07-28 04:02:46 +0200

    temporary disable auto-orient

--- web/uploaders/image.ex
+++ web/uploaders/image.ex
@@ -12,15 +12,15 @@ defmodule SiemensCollection.Image do
   # Define a thumbnail transformation:
 
   def transform(:original, _) do
-    {:convert, "-auto-orient"}
+    {:convert, "-format jpg", :jpg}
   end
 
   def transform(:thumb, _) do
-    {:convert, "-auto-orient -thumbnail 250x250> -gravity center -format jpg", :jpg}
+    {:convert, "-thumbnail 250x250> -gravity center -format jpg", :jpg}
   end
 
   def transform(:popup, _) do
-    {:convert, "-auto-orient -thumbnail 2000x1200> -gravity center -format jpg", :jpg}
+    {:convert, "-thumbnail 2000x1200> -gravity center -format jpg", :jpg}
   end
 
   # Override the persisted filenames:
