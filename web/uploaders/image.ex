92b393cd2780eb0ab0e7fb5253a2d627143e4580
Avael <avaelkross@gmail.com>
2016-07-28 14:13:39 +0200

    wooooooop sweet fn instead of string for transformations

--- web/uploaders/image.ex
+++ web/uploaders/image.ex
@@ -16,11 +16,13 @@ defmodule SiemensCollection.Image do
   end
 
   def transform(:thumb, _) do
-    {:convert, "-auto-orient -thumbnail 250x250> -gravity center -format jpg", :jpg}
+    #{:convert, "-auto-orient -thumbnail 250x250> -gravity center -format jpg", :jpg}
+    {:convert, fn (path, new_path) -> "-size 250x250 #{path} -auto-orient -thumbnail 250x250> -gravity center -format jpg #{new_path}" end}
   end
 
   def transform(:popup, _) do
-    {:convert, "-auto-orient -thumbnail 2000x1200> -gravity center -format jpg", :jpg}
+    {:convert, fn (path, new_path) -> "-size 2000x1200 #{path} -auto-orient -thumbnail 2000x1200> -gravity center -format jpg #{new_path}" end}
+    # {:convert, "-auto-orient -thumbnail 2000x1200> -gravity center -format jpg", :jpg}
   end
 
   # Override the persisted filenames:
